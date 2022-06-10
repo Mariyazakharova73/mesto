650a9b755f3fe37881c0b81c822a739702594f2f
Mariya Zakharova <zakharovamaria73@yandex.ru>
2022-06-10 20:03:21 +0400

    feat: проверка

--- scripts/validate.js
+++ scripts/validate.js
@@ -14,16 +14,13 @@ const hideInputError = (formElement, inputElement, obj) => {
 
 const isValid = (formElement, inputElement, obj) => {
   if (!inputElement.validity.valid) {
-    // Если поле не проходит валидацию, покажем ошибку
     showInputError(formElement, inputElement, inputElement.validationMessage, obj);
   } else {
-    // Если проходит, скроем
     hideInputError(formElement, inputElement, obj);
   }
 };
 
 const hasInvalidInput = (inputList) => {
-  // проходим по этому массиву методом some
   return inputList.some((inputElement) => {
     // Если поле не валидно, колбэк вернёт true
     // Обход массива прекратится и вся функция
@@ -32,7 +29,6 @@ const hasInvalidInput = (inputList) => {
   });
 };
 
-// Функция принимает массив полей ввода и элемент кнопки, состояние которой нужно менять
 const toggleButtonState = (inputList, buttonElement, obj) => {
   // Если есть хотя бы один невалидный инпут
   if (hasInvalidInput(inputList)) {
@@ -46,36 +42,24 @@ const toggleButtonState = (inputList, buttonElement, obj) => {
   }
 };
 
-//Добавление обработчиков всем полям формы
 const setEventListeners = (formElement, obj) => {
-  // Находим все поля внутри формы, сделаем из них массив методом Array.from
   const inputList = Array.from(formElement.querySelectorAll(obj.inputSelector));
-  // Найдём в текущей форме кнопку отправки
   const buttonElement = formElement.querySelector(obj.submitButtonSelector);
   toggleButtonState(inputList, buttonElement, obj);
-  // Обойдём все элементы полученной коллекции
   inputList.forEach((inputElement) => {
-    // каждому полю добавим обработчик события input
     inputElement.addEventListener('input', () => {
-      // Внутри колбэка вызовем isValid, передав ей форму и проверяемый элемент
       isValid(formElement, inputElement, obj);
-      // Вызовем toggleButtonState и передадим ей массив полей и кнопку
       toggleButtonState(inputList, buttonElement, obj);
     });
   });
 };
 
-//Добавление обработчиков всем формам
 const enableValidation = (obj) => {
-  // Найдём все формы с указанным классом в DOM, сделаем из них массив методом Array.from
   const formList = Array.from(document.querySelectorAll(obj.formSelector));
-  // Переберём полученную коллекцию
   formList.forEach((formElement) => {
     formElement.addEventListener('submit', (evt) => {
-      // У каждой формы отменим стандартное поведение
       evt.preventDefault();
     });
-    // Для каждой формы вызовем функцию setEventListeners, передав ей элемент формы
     setEventListeners(formElement, obj);
   });
 };
@@ -86,5 +70,5 @@ enableValidation({
   submitButtonSelector: '.popup__form-button',
   inactiveButtonClass: 'popup__form-button_inactive',
   inputErrorClass: 'popup__form-input_type_error',
-  errorClass: 'popup__input-error_active', //SPAN
+  errorClass: 'popup__input-error_active',
 });
